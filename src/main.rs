845c6f647ea278acf34528d47abd246da7fdb3cb
James Seymour <james.seymour@cubiko.com.au>
2025-06-10 22:26:58 +1000

    feat(cli): init clap cli with basic create and modify commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "waterway",
+    bin_name = "ww",
+    about = "Waterway is a tool for managing stacked diffs",
+    version
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(alias = "c", about = "Create a new branch")]
+    Create {
+        #[arg(short)]
+        message: String,
+    },
+    #[command(alias = "m", about = "Modify an existing branch")]
+    Modify,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.command {
+        Commands::Create { message } => {
+            println!("{:?}", message);
+        }
+        Commands::Modify => {
+            println!("modifying");
+        }
+    }
 }
